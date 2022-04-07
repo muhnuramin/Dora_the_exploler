526f84cecd5c30f4074b8359742112359a3e88f0
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2022-04-07 17:30:33 +0700

    perbaikan query insert dan push

--- application/helpers/push_notification_helper.php
+++ application/helpers/push_notification_helper.php
@@ -1,10 +1,11 @@
 <?php
 
 $FIREBASE_SERVER_KEY = 'AAAAZfo69KE:APA91bE5e2i3uuU72oStelpOKKQL1L-XJ75qT7lH-fbzA5gc14YGVbfUVOEwq7wyqv8ZBY9de8I0G8d1IY-NwlxupOb02shAcayvdhGOo2xp-tuyA0W3m_WyTtW4rz5kJkAS8V7DsaQB';
+// $FIREBASE_SERVER_KEY = 'AAAAqz2xEW8:APA91bEGm03GoS-sVSZpH5-AEndn__RPrH4Qdhat62ERzUcvOK_03YxH6Z_Dr0XGT4em3kDCBX4keBbGf9vMzeSECpkvTcesL5LBSzbA46ijzZatwLZjMpBSuTHO2a_8buugylUv07kt';
 
 define('FCM_AUTH_KEY', $FIREBASE_SERVER_KEY);
 
-function sendPush($to, $title, $body, $icon, $url)
+function sendPush($to, $title, $body, $icon, $url, $type, $id)
 {
     $FIREBASE_TOKEN = $to;
     $postdata = json_encode(
@@ -14,9 +15,13 @@ function sendPush($to, $title, $body, $icon, $url)
                 'title' => $title,
                 'body' => $body,
                 'icon' => $icon,
-                'click_action' => $url
+                'click_action' => $url,
             ],
-            'to' => $FIREBASE_TOKEN
+            'data' => [
+                'type' => $type,
+                'id' => $id
+            ],
+            'to' => $FIREBASE_TOKEN,
         ]
     );
 
@@ -41,3 +46,63 @@ function sendPush($to, $title, $body, $icon, $url)
 // $keyclient = 'eQW_8AV1T5iZUZ8VWGaEh1:APA91bFDJ7xQvPXG6Ev5H8i4cN9Rv7OZmJVMpI6cX2UpZokJoooPSfw3WMOizraSxi_vBWWP_evl8f4t1Bxwx52WmOMTcpJwMk7z0ujq_buZDZ1H1BnrzLBvPsZQohj0BF46Rp8NkG8F';
 
 // sendPush($keyclient, 'Ridlo', 'And this is the text.', 'https://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_28dp.png', 'https://google.co.id/');
+
+// {
+//     "notification": {
+//        "title": "Your Title",
+//        "text": "Your Text",
+//        "click_action": "OPEN_ACTIVITY_1"
+//     },
+//     "data": {
+//        "<some_key>": "<some_value>"
+//     },
+//     "to": "<device_token>"
+//  }
+
+// And with this in your app you can add below code in your activity to be called:
+
+// <intent-filter>
+//     <action android:name="OPEN_ACTIVITY_1" />
+//     <category android:name="android.intent.category.DEFAULT" />
+// </intent-filter>
+
+// function sendGCM($message, $id) {
+
+
+//     $url = 'https://fcm.googleapis.com/fcm/send';
+
+//     $fields = array (
+//             'registration_ids' => array (
+//                     $id
+//             ),
+//             'data' => array (
+//                     "message" => $message
+//             )
+//     );
+//     $fields = json_encode ( $fields );
+
+//     $headers = array (
+//             'Authorization: key=' . "YOUR_KEY_HERE",
+//             'Content-Type: application/json'
+//     );
+
+//     $ch = curl_init ();
+//     curl_setopt ( $ch, CURLOPT_URL, $url );
+//     curl_setopt ( $ch, CURLOPT_POST, true );
+//     curl_setopt ( $ch, CURLOPT_HTTPHEADER, $headers );
+//     curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+//     curl_setopt ( $ch, CURLOPT_POSTFIELDS, $fields );
+
+//     $result = curl_exec ( $ch );
+//     echo $result;
+//     curl_close ( $ch );
+// }
+
+// 
+?>
+
+<!-- $message is your message to send to the device
+
+$id is the devices registration token
+
+YOUR_KEY_HERE is your Server API Key (or Legacy Server API Key) -->
--- application/models/Disposisi_model.php
+++ application/models/Disposisi_model.php
@@ -2,31 +2,29 @@
 
 class Disposisi_model extends CI_model
 {
-    public function riwayatSuratDisposisi($user)
+    public function riwayatSuratDisposisi()
     {
         $filter = array(
-            'd.id_disposisi', 'd.diteruskan_kepada', 'd.tanda_tangan', 'd.tindak_lanjut', 'd.catatan', 'd.catatan_bidang', 'd.diteruskan_oleh', 'd.created_at', 'd.tanggal_dibaca', 'd.dibaca', 'd.tanggal_dikirim',
-            's.asal_surat', 's.nomor_surat', 's.nomor_agenda', 's.tanggal_surat', 's.tanggal_diterima', 's.sifat', 's.perihal', 's.surat'
+            'd.id_disposisi', 'd.diteruskan_kepada', 'd.tanda_tangan', 'd.tindak_lanjut', 'd.catatan', 'd.catatan_bidang', 'd.diteruskan_oleh', 'd.tanggal_dibaca', 'd.dibaca', 'd.tanggal_dikirim',
+            's.id', 's.asal_surat', 's.nomor_surat', 's.nomor_agenda', 's.tanggal_surat', 's.tanggal_diterima', 's.sifat', 's.perihal', 's.surat', 's.dilihat'
         );
 
         $this->db->select($filter);
         $this->db->from('disposisi as d');
         $this->db->join('surat_masuk as s', 's.id=d.id_surat_masuk');
         $this->db->order_by('id_disposisi', 'DESC');
-        $return = $this->db->where('diteruskan_oleh', $user)->get('');
+        // $return = $this->db->where('diteruskan_oleh', $user)->get('');
+        $return = $this->db->get('');
         return $return->result_array();
-
-        // $this->db->from('disposisi');
-        // $query = $this->db->where_in('diteruskan_oleh', $user)->get();
-        // return $query->result_array();
     }
 
     public function suratDisposisiById($id)
     {
         $filter = array(
-            'd.id_disposisi', 'd.diteruskan_kepada', 'd.tanda_tangan', 'd.tindak_lanjut', 'd.catatan', 'd.catatan_bidang', 'd.diteruskan_oleh', 'd.created_at', 'd.tanggal_dibaca', 'd.dibaca', 'd.tanggal_dikirim',
-            's.asal_surat', 's.nomor_surat', 's.nomor_agenda', 's.tanggal_surat', 's.tanggal_diterima', 's.sifat', 's.perihal', 's.surat'
+            'd.id_disposisi', 'd.diteruskan_kepada', 'd.tanda_tangan', 'd.tindak_lanjut', 'd.catatan', 'd.catatan_bidang', 'd.diteruskan_oleh', 'd.tanggal_dibaca', 'd.dibaca', 'd.tanggal_dikirim',
+            's.id', 's.asal_surat', 's.nomor_surat', 's.nomor_agenda', 's.tanggal_surat', 's.tanggal_diterima', 's.sifat', 's.perihal', 's.surat', 's.dilihat'
         );
+
         $this->db->select($filter);
         $this->db->from('disposisi as d');
         $this->db->join('surat_masuk as s', 's.id=d.id_surat_masuk');
@@ -38,9 +36,10 @@ class Disposisi_model extends CI_model
     public function suratDisposisiByUser($id)
     {
         $filter = array(
-            'd.id_disposisi', 'd.diteruskan_kepada', 'd.tanda_tangan', 'd.tindak_lanjut', 'd.catatan', 'd.catatan_bidang', 'd.diteruskan_oleh', 'd.created_at', 'd.tanggal_dibaca', 'd.dibaca', 'd.tanggal_dikirim',
-            's.asal_surat', 's.nomor_surat', 's.nomor_agenda', 's.tanggal_surat', 's.tanggal_diterima', 's.sifat', 's.perihal', 's.surat'
+            'd.id_disposisi', 'd.diteruskan_kepada', 'd.tanda_tangan', 'd.tindak_lanjut', 'd.catatan', 'd.catatan_bidang', 'd.diteruskan_oleh', 'd.tanggal_dibaca', 'd.dibaca', 'd.tanggal_dikirim',
+            's.id', 's.asal_surat', 's.nomor_surat', 's.nomor_agenda', 's.tanggal_surat', 's.tanggal_diterima', 's.sifat', 's.perihal', 's.surat', 's.dilihat'
         );
+
         $this->db->select($filter);
         $this->db->from('disposisi as d');
         $this->db->join('surat_masuk as s', 's.id=d.id_surat_masuk');
