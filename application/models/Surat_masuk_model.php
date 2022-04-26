c9469585fe895a6eacf8579dd5f04ad9e1445eff
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2022-04-26 12:59:19 +0700

    mendapatkan data surat untuk sekretaris, pimpinan

--- application/models/Surat_masuk_model.php
+++ application/models/Surat_masuk_model.php
@@ -4,13 +4,13 @@ class Surat_masuk_model extends CI_model
 {
     public function getAllSurat()
     {
-        if ($this->input->post('user_role') == "Pimpinan") {
+        if ($this->input->post('kondisi_surat') == "N") {
             $this->db->from('surat_masuk');
             $this->db->order_by('id', 'DESC');
             $this->db->where('didisposisi', 'N');
             $query = $this->db->get();
             return $query->result_array();
-        } else if ($this->input->post('user_role') == "Sekretaris") {
+        } else if ($this->input->post('kondisi_surat') == "?") {
             $this->db->from('surat_masuk');
             $this->db->order_by('id', 'DESC');
             $this->db->where('didisposisi', '?');
@@ -22,6 +22,24 @@ class Surat_masuk_model extends CI_model
             $query = $this->db->get();
             return $query->result_array();
         }
+        // if ($this->input->post('user_role') == "Pimpinan") {
+        //     $this->db->from('surat_masuk');
+        //     $this->db->order_by('id', 'DESC');
+        //     $this->db->where('didisposisi', 'N');
+        //     $query = $this->db->get();
+        //     return $query->result_array();
+        // } else if ($this->input->post('user_role') == "Sekretaris") {
+        //     $this->db->from('surat_masuk');
+        //     $this->db->order_by('id', 'DESC');
+        //     $this->db->where('didisposisi', '?');
+        //     $query = $this->db->get();
+        //     return $query->result_array();
+        // } else {
+        //     $this->db->from('surat_masuk');
+        //     $this->db->order_by('id', 'DESC');
+        //     $query = $this->db->get();
+        //     return $query->result_array();
+        // }
     }
 
     public function bacaSurat()
