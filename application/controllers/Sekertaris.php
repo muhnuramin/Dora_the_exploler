49758c61d5453ebe83a6e417e87be6be7fbdb5f8
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2022-05-31 00:28:17 +0700

    Ubah pesan notifikasi

--- application/controllers/Sekertaris.php
+++ application/controllers/Sekertaris.php
@@ -157,7 +157,7 @@ class Sekertaris extends CI_Controller
                     if ($role['nama_role'] == $object) {
                         if ($user['role_id'] == $role['role_id']) {
                             if ($user['fcm_token'] != null) {
-                                sendPush($user['fcm_token'], "Surat Diterima Dari {$data['diteruskan_oleh']}", 'Dari: ' . $surat['asal_surat'], '@mipmap/ic_launcher', $surat['perihal'], 'disposisi', $disposisi_id);
+                                sendPush($user['fcm_token'], "Surat Diterima Dari {$data['diteruskan_oleh']}", 'Surat: ' . $surat['asal_surat'], '@mipmap/ic_launcher', $surat['perihal'], 'disposisi', $disposisi_id);
                                 sleep(1);
                             }
                         }
